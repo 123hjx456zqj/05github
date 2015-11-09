0113afeebd3110c79cf8be0f16d7812184e3edb5
hjx <72566220236@qq.com>
2015-11-09 10:07:31 +0800

    搭建框架

---
+++ 05GitHub/05GitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05GitHub
+//
+//  Created by 何景霞 on 15/11/9.
+//  Copyright © 2015年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05GitHub/05GitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  05GitHub
+//
+//  Created by 何景霞 on 15/11/9.
+//  Copyright © 2015年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
